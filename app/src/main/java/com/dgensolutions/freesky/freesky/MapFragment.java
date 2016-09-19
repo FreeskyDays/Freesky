467bf5471f3a798cdd519956631dad340f42b60e
Ganesh Kaple <gvklive@gmail.com>
2016-09-19 10:36:44 +0530

    Commiting First Time, Tabs Created, Google APi Created,

---
+++ app/src/main/java/com/dgensolutions/freesky/freesky/MapFragment.java
@@ -0,0 +1,30 @@
+package com.dgensolutions.freesky.freesky;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Ganesh Kaple on 17-09-2016.
+ */
+public class MapFragment extends Fragment {
+
+        public MapFragment() {
+            // Required empty public constructor
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_maps, container, false);
+        }
+
+    }
